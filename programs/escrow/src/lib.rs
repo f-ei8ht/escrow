ee97e19fae7d841b749253deb1d27d30b14dc8ca
f-ei8ht <saif7862254@outlook.com>
2025-06-06 16:53:11 +0000

    Add shared.rs in the instruction handler with two helper func

---
+++ programs/escrow/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("AEkEcM7ANVVcmUXLyMKMw89XRfYGz71ZYJJLfziqcN3v");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
