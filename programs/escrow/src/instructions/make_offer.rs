b3d9cc629d790b807f1807650e5d360fa883f59f
f-ei8ht <saif7862254@outlook.com>
2025-06-06 19:48:11 +0000

    Add fn make_offer

--- programs/escrow/src/instructions/make_offer.rs
+++ programs/escrow/src/instructions/make_offer.rs
@@ -17,10 +17,30 @@ pub struct MakeOffer<'info> {
     #[account(mint::token_program = token_program)]
     pub token_mint_b: InterfaceAccount<'info, Mint>,
 
+    #[account(
+       mut,
+       associated_token::mint = token_mint_a,
+       associated_token::authority = maker,
+       associated_token::token_program = token_program
+    )]
     pub maker_token_account_a: InterfaceAccount<'info, TokenAccount>,
 
+    #[account(
+        init, 
+        payer = maker, 
+        space = offer::DISCRIMINATOR.len() + Offer::INIT_SPACE, 
+        seeds = [b"offer", maker.key().as_ref(), id.to_le_bytes().as_ref()],
+        bump
+    )]
     pub offer: Account<'info, Offer>,
 
+    #[account(
+        init,
+        payer = maker,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = offer,
+        associated_token::token_program = token_program
+    )]
     pub vault: InterfaceAccount<'info, TokenAccount>,
 
     pub associated_token_program: Program<'info, AssociatedToken>,
@@ -28,4 +48,33 @@ pub struct MakeOffer<'info> {
     pub token_program: Interface<'info, TokenInterface>,
 
     pub system_program: Program<'info, System>,
+}
+
+pub fn make_offer(
+    context: Context<MakeOffer>,
+    id: u64,
+    token_a_offered_amount: u64,
+    token_b_wanted_amount: u64,
+) -> Result<()> {
+    // Move the tokens from the maker's ATA to the vault
+    transfer_tokens(
+        &context.accounts.maker_token_account_a,
+        &context.accounts.vault,
+        &token_a_offered_amount,
+        &context.accounts.token_mint_a,
+        &context.accounts.maker.to_account_info(),
+        &context.accounts.token_program,
+        None,
+    )?;
+
+    //Save the details of the offer to the offer account
+    context.accounts.offer.set_inner(Offer {
+        id,
+        maker: context.accounts.maker.key(),
+        token_mint_a: context.accounts.token_mint_a.key(),
+        token_mint_b: context.accounts.token_mint_b.key(),
+        token_b_wanted_amount,
+        bump: context.bumps.offer,
+    });
+    Ok(())
 }
