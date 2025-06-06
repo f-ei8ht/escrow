a88319ae1f77ac70779b42f791dc9e429c8684d6
f-ei8ht <saif7862254@outlook.com>
2025-06-06 19:26:48 +0000

    Add constraints make_offer.rs

---
+++ programs/escrow/src/instructions/make_offer.rs
@@ -0,0 +1,31 @@
+use super::shared::transfer_tokens;
+use crate::state::Offer;
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+#[derive(Accounts)]
+pub struct MakeOffer<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+
+    #[account(mint::token_program = token_program)]
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(mint::token_program = token_program)]
+    pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    pub maker_token_account_a: InterfaceAccount<'info, TokenAccount>,
+
+    pub offer: Account<'info, Offer>,
+
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+
+    pub system_program: Program<'info, System>,
+}
